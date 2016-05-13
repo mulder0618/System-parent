d33736ad924a178a4b79b3033c01feeadbe10b75
mulder <mulder@127.0.0.1>
2016-05-13 19:57:56 +0800

    Initial commit

---
+++ System-service/src/main/java/com/mulder/service/base/BaseService.java
@@ -0,0 +1,10 @@
+package com.mulder.service.base;
+
+/**
+ * Created by mulder on 16/5/13.
+ */
+public class BaseService {
+
+
+
+}
---
+++ System-service/src/main/java/com/mulder/service/base/utils/RedisUtil.java
@@ -0,0 +1,52 @@
+package com.mulder.service.base.utils;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Created by mulder on 16/5/13.
+ */
+public class RedisUtil {
+
+    private static JedisPool jedisPool = null;
+    private static RedisUtil redisUtil = null;
+
+    private RedisUtil() {}
+
+    public static RedisUtil getInstance(){
+        if (jedisPool == null) {
+            synchronized (RedisUtil.class) {
+                if (jedisPool == null) {
+                    ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"redis.xml"});
+                    jedisPool = (JedisPool) ctx.getBean("jedisPool");
+                    redisUtil = new RedisUtil();
+                }
+            }
+        }
+        return redisUtil;
+    }
+
+    public void setCacheString(String key,String val)
+    {
+        Jedis client = jedisPool.getResource();
+        client.set(key,val);
+        jedisPool.returnBrokenResource(client);
+    }
+
+
+    public String  getCacheString(String key)
+    {  Jedis client = jedisPool.getResource();
+        String result = client.get(key);
+        jedisPool.returnBrokenResource(client);
+        return  result;
+    }
+
+    public static void main(String[] args){
+        RedisUtil redisUtil = RedisUtil.getInstance();
+        redisUtil.setCacheString("test","^_^");
+        System.out.println(redisUtil.getCacheString("test"));
+    }
+}
