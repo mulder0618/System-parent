c098c406bf722070387b90cccdc647c4d5703ea3
mulder <138111370@qq.com>
2016-05-18 20:20:40 +0800

    动态修改分页拦截器

--- System-mapper/src/main/java/com/mulder/aspect/MultipleDataSourceAspectAdvice.java
+++ System-mapper/src/main/java/com/mulder/aspect/MultipleDataSourceAspectAdvice.java
@@ -1,12 +1,20 @@
 package com.mulder.aspect;
 
+import com.github.miemiedev.mybatis.paginator.OffsetLimitInterceptor;
 import com.mulder.base.MultipleDataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by mulder on 16/5/17.
@@ -19,6 +27,9 @@ public class MultipleDataSourceAspectAdvice {
     @Pointcut("execution(* com.mulder.base.*..*(..))")
     private void daoDirect(){ }//定义一个切入点
 
+    @Resource(name = "sqlSessionFactory")
+    SqlSessionFactory sqlSessionFactory;
+
     /**
      * 决定使用何种数据源
      * @param target
@@ -26,8 +37,14 @@ public class MultipleDataSourceAspectAdvice {
     private void databaseDirect(String target){
         if(target.indexOf("oracle")!=-1){
             MultipleDataSource.setDataSourceKey("oracleDataSource");
+            //动态修改分页拦截器
+            System.out.println(sqlSessionFactory.getConfiguration().getInterceptors());
+            List<Interceptor> mybatisInterceptors = sqlSessionFactory.getConfiguration().getInterceptors();
+            Properties mybatisInterceptorProperties = new Properties();
+            mybatisInterceptorProperties.put("dialectClass","com.github.miemiedev.mybatis.paginator.dialect.OracleDialect");
+            mybatisInterceptors.get(0).setProperties(mybatisInterceptorProperties);
         }
-        else  if(target.indexOf("mysql")!=-1){
+        else if(target.indexOf("mysql")!=-1){
             MultipleDataSource.setDataSourceKey("mysqlDataSource");
         }
     }
