96314c5692e72f82f3b3d9f19cfc85537e2fa0f2
mulder <138111370@qq.com>
2016-06-16 17:46:46 +0800

    单元测试

---
+++ System-web/test/DemoControllerTest.java
@@ -0,0 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import static org.junit.Assert.*;
+
+/**
+ * Created by mulder on 16/6/16.
+ */
+public class DemoControllerTest {
+    File f;
+
+    @Before
+    public void init() {
+        System.out.println("befor test");
+        f=new File("output"+File.separator+"test.xml");
+    }
+
+    @Test(expected=ArithmeticException.class)
+    public void divideZero() {
+        int n=2/0;
+    }
+
+    @Test(timeout = 1)
+    public void timeoutTest() {
+        int i=0;
+        while (i<100000000){
+            i++;
+        }
+    }
+
+    @Test
+    public void assertTest(){
+        int i=0;
+        i++;
+        int expected = 0;
+        assertEquals(expected,i);
+    }
+}
